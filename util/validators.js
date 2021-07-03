f0d817a7b426792c50584e926f878235c41f8d97
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-07-03 14:48:15 +0530

    validating user on login

--- util/validators.js
+++ util/validators.js
@@ -5,10 +5,10 @@ module.exports.validateRegisterInput = (
   confirmPassword
 ) => {
   const errors = {};
-  if (username.trim === '') {
+  if (username.trim() === '') {
     errors.username = 'Username must not be empty';
   }
-  if (email.trim === '') {
+  if (email.trim() === '') {
     errors.email = 'Email must not be empty';
   } else {
     const regEx =
@@ -21,6 +21,21 @@ module.exports.validateRegisterInput = (
     errors.password = 'Password must not be empty';
   } else if (password !== confirmPassword) {
     errors.confirmPassword = 'Password must match';
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {};
+  if (username.trim() === '') {
+    errors.username = 'Username must not be empty';
+  }
+  if (password.trim() === '') {
+    errors.password = 'Password must not be empty';
   }
 
   return {
